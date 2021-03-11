084b69f34affc50704e3721d729d01770f34f309
Maher  Bouidani <maher.buedani@gmail.com>
2021-03-11 01:51:19 -0500

    Weather Component and CSS style

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,13 @@
-// import logo from './logo.svg';
-// import './App.css';
+
 import React from 'react';
-// import { faUnderline } from '@fortawesome/free-solid-svg-icons';
+
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-// import FormHelperText from '@material-ui/core/FormHelperText';
+
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import '../css/Weather.css'
+import { Typography } from '@material-ui/core';
 
 //config file 
 const API_Key = "538882fc8387290c6cee83f313a6acf5";
@@ -24,45 +25,60 @@ class Weather extends React.Component{
     this.getWeather = this.getWeather.bind(this)
   }
 
-  async getWeather(e){
 
-    e.preventDefault();
-    const city = e.target.value;
-    
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`)
-    const data = await response.json();
-    console.log(data)
-    if(response.ok){
-      this.setState({
-        description:data.weather[0].description,
-        temperature:data.main.temp,
-        wind:data.wind.speed,
-    })
-    } else{
-      // console.log('Response Code:'+response.status+'\nError Message:'+ response.statusText)
-      this.setState({errorMessage:"Oops Sorry ! Something has gone wrong, please try again!"})
-  }
+ 
+    async getWeather(e){
 
-  }
+          e.preventDefault();
+          const city = e.target.value;
+          
+          const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_Key}`)
+          const data = await response.json();
+          console.log(data)
+          if(response.ok){
+            this.setState({
+              description:data.weather[0].description,
+              temperature:Math.round(data.main.temp)+'\nC',
+              wind:Math.round(data.wind.speed)+'\nm/sec',
+          })
+          } else{
+            // console.log('Response Code:'+response.status+'\nError Message:'+ response.statusText)
+            this.setState({errorMessage:"Oops Sorry ! Something has gone wrong, please try again!"})
+        }
+
+    }
+  
+  
 
   render(){
     return(
     <div>
-        <FormControl>
-          <InputLabel id="demo-simple-select-label">City</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"  
-              // onChange={this.print}
-              onClick={this.getWeather}
-            >
-              <MenuItem value={'Toronto'}>Toronto</MenuItem>
-              <MenuItem value={'Ottawa'}>Ottawa</MenuItem>
-              <MenuItem value={'Tokyo'}>Tokyo</MenuItem>
-            </Select>
-        </FormControl>
+      <div>
+          <FormControl>
+            <InputLabel variant='standard'>City</InputLabel>
+              <Select
+                className="select-city"
+                onChange={this.getWeather}
+                autoFocus
+              >
+                <MenuItem value={'Toronto'}>Toronto</MenuItem>
+                <MenuItem value={'Ottawa'}>Ottawa</MenuItem>
+                <MenuItem value={'Tokyo'}>Tokyo</MenuItem>
+              </Select>
+          </FormControl>
+      </div>
+     { this.state.description && <div className="select-result">
+        <Typography color='textPrimary' variant='body1'>Clouds</Typography>
+        <Typography color='textPrimary' variant='body2'>{this.state.description}</Typography> 
+      </div>
+     }
+     <div className="select-result"> 
+     <Typography color='textPrimary' variant='body2'>{this.state.temperature}</Typography> 
+     {this.state.wind && <Typography color='textPrimary' variant='body2'>Wind {this.state.wind}</Typography>}
+     </div>
+  
+
     </div>
-      
 
     )
   }
